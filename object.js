53cf2839944dc0743c617011d1cf783e7e85711b
tunderdomb <tunderdomb@gmail.com>
2015-09-19 12:39:15 +0200

    new descriptor interface

--- object.js
+++ object.js
@@ -1,38 +1,5 @@
-var object = module.exports = {}
-
-object.accessor = function (obj, name, get, set) {
-  Object.defineProperty(obj, name, {
-    get: get,
-    set: set
-  })
-}
-
-object.defineGetter = function (obj, name, fn) {
-  Object.defineProperty(obj, name, {
-    get: typeof fn == "function" ? fn : function () {
-      return fn
-    }
-  })
-}
-
-object.defineSetter = function (obj, name, fn) {
-  Object.defineProperty(obj, name, {
-    set: fn
-  })
-}
-
-object.method = function (obj, name, fn) {
-  Object.defineProperty(obj, name, {
-    value: fn
-  })
-}
-
-object.property = function (obj, name, fn) {
-  Object.defineProperty(obj, name, {
-    value: fn,
-    configurable: true
-  })
-}
+var Descriptor = require("./Descriptor")
+var object = module.exports = new Descriptor()
 
 object.for = function( obj, callback ){
   for( var prop in obj ){
