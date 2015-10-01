85f9fc4bdecdad722838e91dc9375f56d0f43f96
tunderdomb <tunderdomb@gmail.com>
2015-10-01 12:01:24 +0200

    object.for, object.in

--- object.js
+++ object.js
@@ -4,7 +4,16 @@ var object = module.exports = new Descriptor()
 object.for = function( obj, callback ){
   for( var prop in obj ){
     if( obj.hasOwnProperty(prop) ){
-      callback(prop, obj)
+      callback(obj[prop], prop, obj)
+    }
+  }
+  return obj
+}
+
+object.in = function( obj, callback ){
+  for( var prop in obj ){
+    if( obj.hasOwnProperty(prop) ){
+      callback(prop, obj[prop], obj)
     }
   }
   return obj
